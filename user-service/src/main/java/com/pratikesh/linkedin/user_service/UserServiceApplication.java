24bfdb6644c14ea56a77266d18e7c5ad914a6930
Pratikesh <pratikeshb9@gmail.com>
2025-02-27 18:22:24 +0530

    Initial Commit

---
+++ user-service/src/main/java/com/pratikesh/linkedin/user_service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pratikesh.linkedin.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ user-service/src/test/java/com/pratikesh/linkedin/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratikesh.linkedin.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
